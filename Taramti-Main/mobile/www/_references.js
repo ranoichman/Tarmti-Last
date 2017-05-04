8477d3d5c4f059ad21f288a5061942f57091676b
ranoichman <ran.oichman@gmail.com>
2017-05-04 11:01:53 +0300

    Hope

---
+++ Taramti-Main/mobile/www/_references.js
@@ -0,0 +1,7 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="bootstrap.js" />
+/// <reference path="jquery-1.10.2.js" />
+/// <reference path="modernizr-2.6.2.js" />
+/// <reference path="react/jsxtransformer-0.13.1.js" />
+/// <reference path="react/react-0.13.1.js" />
+/// <reference path="react/react-with-addons-0.13.1.js" />
---
+++ Taramti-Main/mobile/www/amutaSignUpRan.jsx
@@ -0,0 +1,33 @@
+﻿
+        
+
+
+        var SignUp = React.createClass({
+            
+            edit: function(){
+                this.setState({editing})
+            },
+                
+            render: function () {
+                return (
+                <div className="row">
+                    <div className="col-xs-pull-6" dir="rtl">
+                        <div style={{ width: '50%' }}>
+                        <div className="rect">
+                        <h3 className="text-center">15:20</h3>
+                    </div>
+                    <h4 className="text-center">כותרת</h4>
+                    <p className="descPar">קצת מלל וכל מיני דברים שבא לי לכתוב וכן הנה עוד קצת דברים ותכף יהיו כאן גם תמונות וזה בסך הכל תיאור של מוצר</p>
+                </div>
+            </div>
+            <div className="col-xs-6">
+                <img ref="disIMG" src={this.state.imgArr[this.state.index]} />
+            </div>
+        </div>
+        );
+}
+});
+
+ReactDOM.render(
+<Auction />,
+document.getElementById('auctionsPH'));
